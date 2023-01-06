76255bde8ccdc4423d4888a72fccaf7fefe01918
ggujangi <asmwj3@naver.com>
2023-01-06 16:31:52 +0900

    [#2 BottomSheetDialog] 업로드 다이얼로그 메인 화면 적용

--- app/src/main/java/com/ggu/share/MainActivity.kt
+++ app/src/main/java/com/ggu/share/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.ggu.share
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.ggu.share.databinding.ActivityMainBinding
+import com.ggu.share.upload.ui.UploadDialogFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,5 +15,17 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initView()
+    }
+
+
+    private fun initView() {
+        binding.uploadButton.setOnClickListener(this)
+    }
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.upload_button -> UploadDialogFragment.newInstance().show(supportFragmentManager, UploadDialogFragment.TAG)
+        }
     }
 }
